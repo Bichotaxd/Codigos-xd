dafaae8696cc06c7a4ee787bb7d4bfb1f8e58a5c
Bichotaxd <153379745+Bichotaxd@users.noreply.github.com>
2023-12-09 10:04:39 -0600

    Add files via upload

---
+++ Sumas.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace;
+
+int main(){
+	int num1, num2, resultado;
+	
+	cout<<"digite el primer numero: ";
+	cin>>num1;
+	
+	cout<<"digite el segundo numero: ";
+	cin>>num2;
+	
+	resultado=num1*num2
+	
+	cout<<"la suma es: "<<resultado;
+	
+}
